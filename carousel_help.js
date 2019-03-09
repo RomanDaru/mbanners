d772a203af649bd1217f9505a953b018e0190296
Roman <roman.daru.ml@gmail.com>
2019-03-09 19:12:16 +1100

    First Commit

---
+++ carousel_help.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  $('#mainBanner').on('slide.bs.carousel', function (e) {
+    var forSlide = $('.for-slide-' + $(e.relatedTarget).index());
+    if ( !forSlide.hasClass('in') ) {
+      $('#collapseGroup>.collapse.in').collapse('hide');
+      forSlide.collapse('show');
+    }
+  })
+});
